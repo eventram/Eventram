c9af51cd70da442c947f1365fe9021809a1e39ef
Masato Hirose <malohithon@masato-MBP.local>
2014-01-11 11:51:02 +0900

    malohithon - rails 導入

---
+++ eventram/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Eventram::Application.config.secret_key_base = 'e62ed06a30fd856ecfed6bc740683a380dbfe565c884864e550f7041a6ae1edf54dab7dc37a21fcff667a7e012c5f442e6989dd72e240c1f532648270a19c310'
